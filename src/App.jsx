67934b7920546ce1ec97d726f42a46a90f9547d0
Satoshi Matsushima <>
2021-07-05 23:30:31 +0900

    markup coding and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,49 @@
-import './style.css';
+import './styles.css';
 
 export const App = () => {
-  return <div></div>;
+  return (
+    <>
+      <h1 className="main-title">TODO LIST</h1>
+      <div className="input-area">
+        <input placeholder="TODOを入力" />
+        <button>追加</button>
+      </div>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          <li className="list-row">
+            <div>
+              <span>aaaaa</span>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+          <li className="list-row">
+            <div>
+              <span>bbbb</span>
+              <button>完了</button>
+              <button>削除</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+      <div className="complete-area">
+        <h2 className="title">完了のTODO</h2>
+        <ul>
+          <li className="list-row">
+            <div>
+              <span>cccc</span>
+              <button>戻す</button>
+            </div>
+          </li>
+          <li className="list-row">
+            <div>
+              <span>dddd</span>
+              <button>戻す</button>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
 };
