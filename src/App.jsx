e49c421a2faacd1cedb305391afd69659f37f61e
Satoshi Matsushima <>
2021-07-06 22:51:09 +0900

    creating function for adding todo

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,29 @@ import { useState } from 'react';
 import './styles.css';
 
 export const App = () => {
+  const [todoText, setTodoText] = useState('');
   const [incompleteTodos, setIncompleteTodos] = useState(['aaaaa', 'bbbbb']);
   const [completeTodos, setCompleteTodos] = useState(['cccc', 'dddd']);
+
+  const onchangeTodoText = (event) => setTodoText(event.target.value);
+
+  const onClickAdd = () => {
+    if (todoText === '') return;
+    const newTodos = [...incompleteTodos, todoText];
+    setIncompleteTodos(newTodos);
+    setTodoText('');
+  };
 
   return (
     <>
       <h1 className="main-title">TODO LIST</h1>
       <div className="input-area">
-        <input placeholder="TODOを入力" />
-        <button>追加</button>
+        <input
+          placeholder="TODOを入力"
+          value={todoText}
+          onChange={onchangeTodoText}
+        />
+        <button onClick={onClickAdd}>追加</button>
       </div>
       <div className="incomplete-area">
         <p className="title">未完了のTODO</p>
